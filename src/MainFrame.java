69a691df5212f2bedf4ca3ac942d5244bedc4f60
Dmitriy <112623422+CyM4aTblu@users.noreply.github.com>
2022-10-07 01:32:23 +0300

    added a changing slides func

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,4 +1,3 @@
-import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -7,37 +6,37 @@ public class MainFrame extends JFrame {
     //---- GLOBAL ELEMENTS ------
     CardLayout crd;
     JPanel menu, about, review;
-    Container box;
+    Container boxWithMenuSectionPanels;
     // ---- ELEMENTS OF REVIEW SCREEN ----
 
-    CardLayout reviewCrd;
-    JPanel rw1,rw2,rw3,rw4,rw5,rw6,rw7;
-    Container reviewSlides;
+//    CardLayout reviewCrd;
+//    JPanel rw1,rw2,rw3,rw4,rw5,rw6,rw7;
+//    Container reviewSlides;
 
     MainFrame() {
-        box = getContentPane();// set up for screens panels containers
+        boxWithMenuSectionPanels = getContentPane();// set up for screens panels containers
         crd = new CardLayout();
-        box.setLayout(crd);
+        boxWithMenuSectionPanels.setLayout(crd);
 
         createMenu();//calling constructors of screen panels
         createAbout();
         createReview();
-        setRw1();
+        //setRw1();
 
-        box.add("menu", menu);//adding elements in container
-        box.add("about", about);
-        box.add("review", review);
+        boxWithMenuSectionPanels.add("menu", menu);//adding elements in container
+        boxWithMenuSectionPanels.add("about", about);
+        boxWithMenuSectionPanels.add("review", review);
         //box.add("rw1", rw1);
 
         //----------------------------------------------------------------------------
 
-        reviewSlides = getContentPane();// set up for slides
-        reviewCrd = new CardLayout();
-        reviewSlides.setLayout(reviewCrd);
-
-        setRw1();
-
-        reviewSlides.add("1", rw1);
+//        reviewSlides = getContentPane();// set up for slides
+//        reviewCrd = new CardLayout();
+//        reviewSlides.setLayout(reviewCrd);
+//
+//        setRw1();
+//
+//        reviewSlides.add("1", rw1);
 
         //---------------------------------------------------------------------------
 
@@ -53,6 +52,32 @@ public class MainFrame extends JFrame {
 
     }
 
+    private JPanel createBottomBar(boolean isBackButton){
+        JPanel btmBar = new JPanel();
+        btmBar.setLayout(null);
+        btmBar.setBounds(0, 605 , 1280, 75);
+        btmBar.setBackground(topBtmBarBackground);
+
+        if(isBackButton) {
+            JButton returnToMenu = new JButton();
+            returnToMenu.setFocusable(false);
+            ImageIcon img = new ImageIcon(".\\src\\ReturnArrow.png");
+            returnToMenu.setBounds(50, 15, 100, 45);
+            returnToMenu.setIcon(img);
+            returnToMenu.setContentAreaFilled(false);
+            returnToMenu.setFocusPainted(false);
+            returnToMenu.setBorderPainted(false);
+            returnToMenu.addActionListener(new AbstractAction() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    crd.show(boxWithMenuSectionPanels, "menu");
+
+                }
+            });
+            btmBar.add(returnToMenu);
+        }
+        return btmBar;
+    }
     private void createMenu() {
         menu = new JPanel();
         menu.setLayout(null);
@@ -67,65 +92,40 @@ public class MainFrame extends JFrame {
 
         JLabel txt = new JLabel();
         txt.setText("<html>Автоматизированный комплекс<br>для проведения акустических<br>и виброакустических измерений<br>''ШЕПОТ''</html>");
-        txt.setForeground(new Color(0x76AFD0));
+        txt.setForeground(textColor);
         txt.setFont(new Font("Calibri", Font.BOLD, 40 ));
         //txt.setVerticalAlignment(JLabel.TOP);
         txt.setBounds(500, 0, 700, 200);
         menu.add(txt);
 
         JPanel buttons = new JPanel();
-        buttons.setBackground(new Color(200, 243, 255, 255));
+        buttons.setBackground(backGround);
         buttons.setBounds(550, 250, 650, 350);
         //buttons.setLayout(new GridLayout(2, 1));
         menu.add(buttons);
         buttons.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
         buttons.setLayout(new GridLayout(3, 3, 10, 10));
 
-        JButton rewiewBtn = new JButton("Обзор");
-        rewiewBtn.setFocusable(false);
-        rewiewBtn.setFont(new Font("Calibri", Font.BOLD, 50 ));
-        rewiewBtn.setForeground(new Color(66, 122, 171, 134));
-        rewiewBtn.setBounds(25, 10, 280, 90);
-        rewiewBtn.setBackground(new Color(162, 226, 245));
+        JButton rewiewBtn = getMainMenuButton("Обзор",345, 220, 280, 90);
         rewiewBtn.addActionListener(new AbstractAction() {
             @Override
-            public void actionPerformed(ActionEvent e) { crd.show(box, "review");
+            public void actionPerformed(ActionEvent e) { crd.show(boxWithMenuSectionPanels, "review");
 
             }
         });
         buttons.add(rewiewBtn);
 
-        JButton contBtn = new JButton("Состав");
-        contBtn.setFocusable(false);
-        contBtn.setFont(new Font("Calibri", Font.BOLD, 50 ));
-        contBtn.setForeground(new Color(66, 122, 171, 134));
-        contBtn.setBounds(345, 10, 280, 90);
-        contBtn.setBackground(new Color(162, 226, 245));
+        JButton contBtn = getMainMenuButton("Состав",345, 220, 280, 90);
         buttons.add(contBtn);
 
-        JButton lrnBtn = new JButton("Обучение");
-        lrnBtn.setFocusable(false);
-        lrnBtn.setFont(new Font("Calibri", Font.BOLD, 50 ));
-        lrnBtn.setForeground(new Color(66, 122, 171, 134));
-        lrnBtn.setBounds(25, 110, 280, 90);
-        lrnBtn.setBackground(new Color(162, 226, 245));
+        JButton lrnBtn = getMainMenuButton("Обучение",345, 220, 280, 90);
         buttons.add(lrnBtn);
 
-        JButton hlpBtn = new JButton("Справка");
-        hlpBtn.setFocusable(false);
-        hlpBtn.setFont(new Font("Calibri", Font.BOLD, 50 ));
-        hlpBtn.setForeground(new Color(66, 122, 171, 134));
-        hlpBtn.setBounds(345, 110, 280, 90);
-        hlpBtn.setBackground(new Color(162, 226, 245));
+        JButton hlpBtn = getMainMenuButton("Справка",345, 220, 280, 90);
         buttons.add(hlpBtn);
 
-        JButton abtBtn = new JButton("О программе");
-        abtBtn.setFocusable(false);
-        abtBtn.setFont(new Font("Calibri", Font.BOLD, 35 ));
-        abtBtn.setForeground(new Color(66, 122, 171, 134));
-        abtBtn.setBounds(25, 220, 280, 90);
-        abtBtn.setBackground(new Color(162, 226, 245));
-        //abtBtn.addActionListener(e -> About());
+        JButton abtBtn = getMainMenuButton("О программе",345, 220, 280, 90);
+        abtBtn.setFont(new Font("Calibri", Font.BOLD, 40 ));
         abtBtn.addActionListener(new AbstractAction() {
             @Override
             public void actionPerformed(ActionEvent e) { crd.show(box, "about");
@@ -144,85 +144,56 @@ public class MainFrame extends JFrame {
         buttons.add(extBtn);
     }
 
+    private JButton getMainMenuButton(String text, int x, int y, int width, int height) {
+        JButton btn = new JButton(text);
+        btn.setFocusable(false);
+        btn.setFont(new Font("Calibri", Font.BOLD, 50 ));
+        btn.setForeground(buttonForeground); // TODO: bring colour from global variables
+        btn.setBounds(x, y, width, height);
+        btn.setBackground(buttonBackground);
+        return btn;
+    }
     private void createAbout() {
         about = new JPanel();
         about.setLayout(null);
-        about.setBackground(new Color(224, 255, 255));
+        about.setBackground(backGround);
 
         JPanel topBar = new JPanel();
         topBar.setBounds(0, 0 , 1280, 75);
-        topBar.setBackground(new Color(197, 230, 236));
+        topBar.setBackground(topBtmBarBackground);
         about.add(topBar);
 
         JLabel txt = new JLabel();
         txt.setText("О программе");
         txt.setFont(new Font("Calibri", Font.BOLD, 70 ));
         txt.setHorizontalTextPosition(JLabel.CENTER);
-        txt.setForeground(new Color(140, 184, 225));
+        txt.setForeground(textColor);
         topBar.add(txt);
 
-        JPanel btmBar = new JPanel();
-        btmBar.setLayout(null);
-        btmBar.setBounds(0, 605 , 1280, 75);
-        btmBar.setBackground(new Color(197, 230, 236));
+        JPanel btmBar = createBottomBar(true);
         about.add(btmBar);
-
-        JButton returnToMenu = new JButton();
-        returnToMenu.setFocusable(false);
-        ImageIcon img = new ImageIcon(".\\src\\ReturnArrow.png");
-        returnToMenu.setBackground(new Color(209, 241, 255));
-        returnToMenu.setBounds(50, 15, 100, 45);
-        returnToMenu.setIcon(img);
-        returnToMenu.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) { crd.show(box, "menu");
-
-            }
-        });
-        btmBar.add(returnToMenu);
     }
 
     private void createReview() {
-
-
-        //-----------------------------------------------------------------------
-
         review = new JPanel();
         review.setLayout(null);
-        review.setBackground(new Color(224, 255, 255));
+        review.setBackground(backGround);
 
-        JPanel btmBar = new JPanel();
-        btmBar.setLayout(null);
-        btmBar.setBounds(0, 605 , 1280, 75);
-        btmBar.setBackground(new Color(209, 241, 255));
+        JPanel btmBar = createBottomBar(true);
         review.add(btmBar);
-
-        JButton returnToMenu = new JButton();
-        returnToMenu.setFocusable(false);
-        ImageIcon img = new ImageIcon(".\\src\\ReturnArrow.png");
-        returnToMenu.setBounds(50, 15, 100, 45);
-        returnToMenu.setIcon(img);
-        returnToMenu.setContentAreaFilled(false);
-        returnToMenu.setFocusPainted(false);
-        returnToMenu.setBorderPainted(false);
-        returnToMenu.addActionListener(new AbstractAction() {
-            @Override
-            public void actionPerformed(ActionEvent e) { crd.show(box, "menu");
-
-            }
-        });
-        btmBar.add(returnToMenu);
 
         ImageIcon nxt = new ImageIcon(".\\src\\ArrowNext.png");
         JButton nextSlide = new JButton(nxt);
         nextSlide.setFocusable(false);
-        //nextSlide.setBackground(new Color(209, 241, 255));
         // mid X = 640
         nextSlide.setBounds(680, 5, 65, 65);
         nextSlide.addActionListener(new AbstractAction() {
             @Override
-            public void actionPerformed(ActionEvent e) { reviewCrd.show(reviewSlides, "1");
-
+            public void actionPerformed(ActionEvent e) {
+                JPanel page = reviewPage(1);
+                review.add(page);
+                page.repaint();
+                review.repaint();
             }
         });
         nextSlide.setContentAreaFilled(false);
@@ -231,10 +202,85 @@ public class MainFrame extends JFrame {
         btmBar.add(nextSlide);
     }
 
-    private void setRw1(){
-        rw1 = new JPanel();
-        rw1.setLayout(null);
-        rw1.setBackground(new Color(221, 255, 209));
-        rw1.setBounds(0,75 , 1280, 500);
+    private JPanel reviewPage(int numberOfPage){
+        switch (numberOfPage){
+            case 1:
+                JPanel page = new JPanel();
+                page.setLayout(null);
+                page.setBackground(backGround);
+                page.setBounds(0,0 , 1280, 605);
+                JPanel topBar = new JPanel();
+                topBar.setBounds(0, 0 , 1280, 75);
+                topBar.setBackground(topBtmBarBackground);
+                page.add(topBar);
+
+                JLabel txt = new JLabel();
+                txt.setText("Спрут-6МА");
+                txt.setFont(new Font("Calibri", Font.BOLD, 70 ));
+                txt.setHorizontalTextPosition(JLabel.CENTER);
+                txt.setForeground(textColor);
+                topBar.add(txt);
+                return page;
+            default:
+                return new JPanel();
+        }
     }
+
+//    private void setRw1(){
+//        rw1 = new JPanel();
+//        rw1.setLayout(null);
+//        rw1.setBackground(new Color(221, 255, 209));
+//        rw1.setBounds(0,75 , 1280, 500);
+//    }
+    
+//    private JPanel getBaseReviewSlide(boolean setRightButton, boolean setLeftButton, AbstractAction rBtnAction, AbstractAction lBtnAction) {
+//        JPanel baseSlide = new JPanel();
+//        baseSlide.setLayout(null);
+//        baseSlide.setBackground(new Color(224, 255, 255));
+//
+//        JPanel btmNavBar = new JPanel();
+//        btmNavBar.setLayout(null);
+//        btmNavBar.setBounds(0, 605 , 1280, 75);
+//        btmNavBar.setBackground(new Color(209, 241, 255));
+//        baseSlide.add(btmNavBar);
+//
+//        JButton returnToMenu = new JButton();
+//        returnToMenu.setFocusable(false);
+//        ImageIcon img = new ImageIcon(".\\src\\ReturnArrow.png");
+//        returnToMenu.setBounds(50, 15, 100, 45);
+//        returnToMenu.setIcon(img);
+//        returnToMenu.setContentAreaFilled(false);
+//        returnToMenu.setFocusPainted(false);
+//        returnToMenu.setBorderPainted(false);
+//        returnToMenu.addActionListener(new AbstractAction() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) { crd.show(box, "menu");
+//
+//            }
+//        });
+//        btmNavBar.add(returnToMenu);
+//
+//        return baseSlide;
+//    }
+//
+//    private AbstractAction showSlide(String slideId) {
+//        crd.show(box, slideId);
+//        return null;
+//    }
+
+
+
+//    JPanel slide1 = getBaseReviewSlide(true, true,
+//                new AbstractAction() {
+//                    @Override
+//                    public void actionPerformed(ActionEvent e) {
+//
+//                    }
+//                },
+//                new AbstractAction() {
+//                    @Override
+//                    public void actionPerformed(ActionEvent e) {
+//
+//                    }
+//                });
 }
